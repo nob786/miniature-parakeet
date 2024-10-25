a3ecdd467f00f5c9560d2363312b1cc5b3550395
AmirKhan732 <amirabdulmannanstudent@gmail.com>
2024-10-25 16:16:13 +0500

    Added 2 new files

---
+++ APP.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const APP = () => {
+  return (
+    <View>
+      <Text>APP</Text>
+      <Text>this is made by amir khan and practice by it</Text>
+    </View>
+  )
+}
+
+export default APP
+
+const styles = StyleSheet.create({})
